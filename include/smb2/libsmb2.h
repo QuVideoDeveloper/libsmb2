53569d39de3fe7bdf4109f9b034be27f88915042
徐新元 <xinyuan.xu@quvideo.com>
2024-06-11 15:03:52 +0800

    fix: visionOS 2.0 build error

--- include/smb2/libsmb2.h
+++ include/smb2/libsmb2.h
@@ -19,6 +19,11 @@
 #ifndef _LIBSMB2_H_
 #define _LIBSMB2_H_
 
+#ifdef __APPLE__ /* Some platforms goes well without stdint.h include */
+#include <stdint.h>
+#include <stddef.h>
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -53,8 +58,8 @@ struct smb2_stat_64 {
         uint64_t smb2_mtime_nsec;
         uint64_t smb2_ctime;
         uint64_t smb2_ctime_nsec;
-    uint64_t smb2_btime;
-    uint64_t smb2_btime_nsec;
+        uint64_t smb2_btime;
+        uint64_t smb2_btime_nsec;
 };
 
 struct smb2_statvfs {
@@ -1008,7 +1013,7 @@ int smb2_echo(struct smb2_context *smb2);
  * separation between dcerpc and smb2, so we need to include this header
  * here to retain compatibility for apps that depend on those symbols.
  */
-#include <smb2/libsmb2-dcerpc-srvsvc.h>
+#include "libsmb2-dcerpc-srvsvc.h"
 
 #ifdef __cplusplus
 }
--- include/smb2/smb2.h
+++ include/smb2/smb2.h
@@ -19,15 +19,19 @@
 #ifndef _SMB2_H_
 #define _SMB2_H_
 
-#include <smb2/smb2-errors.h>
+#ifdef __APPLE__ /* Some platforms goes well without stdint.h include */
+#include <sys/_types/_time_t.h>
+#endif
+
+#include "smb2-errors.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 struct smb2_timeval {
-        uint32_t tv_sec;
-        uint32_t tv_usec;
+        time_t tv_sec;
+        uint64_t tv_usec;
 };
 
 #define SMB2_ERROR_REPLY_SIZE 9
@@ -479,6 +483,7 @@ struct smb2_read_reply {
 #define SMB2_FILE_BASIC_INFORMATION             0x04
 #define SMB2_FILE_STANDARD_INFORMATION          0x05
 #define SMB2_FILE_RENAME_INFORMATION            0x0a
+#define SMB2_FILE_DISPOSITION_INFORMATION       0x0d
 #define SMB2_FILE_ALL_INFORMATION               0x12
 #define SMB2_FILE_END_OF_FILE_INFORMATION       0x14
 
@@ -559,6 +564,13 @@ struct smb2_query_info_request {
  */
 struct smb2_file_end_of_file_info {
         uint64_t end_of_file;
+};
+
+/*
+ * FILE_DISPOSITION_INFORMATION.
+ */
+struct smb2_file_disposition_info {
+        uint8_t delete_pending;
 };
 
 /*
@@ -647,11 +659,11 @@ struct smb2_ace {
          * SMB2_DENIED_ALLOWED_CALLBACK_ACE_TYPE,
          * SMB2_SYSTEM_RESOURCE_ATTRIBUTE_ACE_TYPE
          */
-        int   ad_len;
+        size_t   ad_len;
         char *ad_data;
 
         /* raw blob, used for unknown ACE types */
-        int   raw_len;
+        size_t   raw_len;
         char *raw_data;
 };
 
